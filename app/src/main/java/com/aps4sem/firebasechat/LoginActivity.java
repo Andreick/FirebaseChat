96573a26d3cd41bb0a299a9bb33d73715447bfdd
Andreick <andreick.gomes.ismart@gmail.com>
2021-05-01 14:12:36 -0300

    Cria um prototipo da tela de login

--- app/src/main/java/com/aps4sem/firebasechat/LoginActivity.java
+++ app/src/main/java/com/aps4sem/firebasechat/LoginActivity.java
@@ -2,13 +2,40 @@ package com.aps4sem.firebasechat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editPassword;
+    private Button buttonEnter;
+    private TextView textRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
+        buttonEnter = findViewById(R.id.button_enter);
+        textRegister = findViewById(R.id.text_register);
+
+        buttonEnter.setOnClickListener(v -> {
+            String email = editEmail.getText().toString();
+            String password = editPassword.getText().toString();
+
+            Log.i("Teste", email);
+            Log.i("Teste", password);
+        });
+
+        textRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
